61809235bd538054929047e2802adee69af284b9
Mitchell Kossoris <mkossoris@gmail.com>
2018-02-11 09:55:32 -0600

    Added some CSS and npm start script

--- app.js
+++ app.js
@@ -28,19 +28,19 @@ app.listen(8000, function(req, res) {
 
 // API STUFF
 
-// function requestMeme(memeInput) {
-//     const GoogleImages = require('google-images');
- 
-//     const client = new GoogleImages('014316724742280594798:nfjy14mes6c', 'AIzaSyC0Zdeyc2_4_O3v48CgT1bVnHkfQzt6AJc');
-    
-//     client.search(memeInput + " meme")
-//         .then(images => {
-//             var rand = Math.floor(Math.random() * Math.floor(images.length));
-//             memeUrl = images[rand]["url"];
-//             console.log("rand = " + rand + "\n" + "memeUrl = " + memeUrl);
-//         });
-// }
-
 function requestMeme(memeInput) {
-    memeUrl = "http://i0.kym-cdn.com/photos/images/facebook/001/217/729/f9a.jpg";
+    const GoogleImages = require('google-images');
+ 
+    const client = new GoogleImages('014316724742280594798:nfjy14mes6c', 'AIzaSyC0Zdeyc2_4_O3v48CgT1bVnHkfQzt6AJc');
+    
+    client.search(memeInput + " meme")
+        .then(images => {
+            var rand = Math.floor(Math.random() * Math.floor(images.length));
+            memeUrl = images[rand]["url"];
+            console.log("rand = " + rand + "\n" + "memeUrl = " + memeUrl);
+        });
 }
+
+// function requestMeme(memeInput) {
+//     memeUrl = "http://i0.kym-cdn.com/photos/images/facebook/001/217/729/f9a.jpg";
+// }
